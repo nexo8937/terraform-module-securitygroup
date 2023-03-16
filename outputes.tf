592ce3af217656d4534d328cf5bd8b646687ff8a
Yeghishe <marsel8937@gmail.com>
2023-03-16 17:11:53 +0000

    adding autoscaling output

--- outputes.tf
+++ outputes.tf
@@ -5,6 +5,11 @@ output "db-sg" {
 output "lb-sg" {
   value = aws_security_group.lb-sg.id
 }
+
  output "image-sg" {
   value = aws_security_group.image-sg.id
+}
+
+output "autoscaling-sg" {
+  value = aws_security_group.web-server.id
 }
