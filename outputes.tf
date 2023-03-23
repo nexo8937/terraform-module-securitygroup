020847461ac1b2121024e0205c7ce86dc5eead16
Yeghishe <marsel8937@gmail.com>
2023-03-23 11:30:36 +0000

    asd

--- outputes.tf
+++ outputes.tf
@@ -2,9 +2,9 @@ output "db-sg" {
   value = aws_security_group.rds.id
 }
 
-output "lb-sg" {
-  value = aws_security_group.lb-sg.id
-}
+#output "lb-sg" {
+#  value = aws_security_group.lb-sg.id
+#}
 
  output "image-sg" {
   value = aws_security_group.image-sg.id
