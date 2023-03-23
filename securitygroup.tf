3f930efd0c0b0b57f7f5bcae340b8d5da5176935
Yeghishe <marsel8937@gmail.com>
2023-03-23 11:05:27 +0000

    changing

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,32 +1,32 @@
 #LOAD BALANCER SECURITY GROUP
-resource "aws_security_group" "lb-sg" {
-  name        = "load balancer sg"
-  vpc_id      = var.vpc
-  description = "Allow HTTP and HTTPS traffic"
+#resource "aws_security_group" "lb-sg" {
+#  name        = "load balancer sg"
+#  vpc_id      = var.vpc
+#  description = "Allow HTTP and HTTPS traffic"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  ingress {
+#    from_port   = 443
+#    to_port     = 443
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "Load Balances sg"
-  }
-}
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  tags = {
+#    Name = "Load Balances sg"
+#  }
+#}
 
 
 #WEB SERVER SECURITY GROUP
