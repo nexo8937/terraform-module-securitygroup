87a182dc3df93f3f70540eb61286e295db631f9b
Yeghishe <marsel8937@gmail.com>
2023-03-24 19:51:06 +0000

    removin all security groups

--- outputes.tf
+++ outputes.tf
@@ -1,15 +1,3 @@
-#output "db-sg" {
-#  value = aws_security_group.rds.id
-#}
-
-#output "lb-sg" {
-#  value = aws_security_group.lb-sg.id
-#}
-
- output "image-sg" {
+output "image-sg" {
   value = aws_security_group.image-sg.id
 }
-
-#output "autoscaling-sg" {
-#  value = aws_security_group.web-server.id
-#}
